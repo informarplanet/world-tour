8520a131d5c6577d28dc62c73a04fe2dc6b57b57
Dead Army <pulakp18@gmail.com>
2024-05-16 17:40:22 +0530

    updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Nav(){
     return(
         <div>
             <ul>
-                <a href="/home">Home</a>
-                <a href="/about">About Us</a>
-                <a href="/contact">Contact Us</a>
+                <Link to='/home'>Home</Link>
+                <Link to='/about'>About Us</Link>
+                <Link to='/contact'>Contact Us</Link>
             </ul>
         </div>
     )
